3a6e77d4953d5991078f0340ea8a34484c02b361
Vance-M <myveryvills@yahoo.com>
2021-01-26 08:30:38 -0800

    fix results page with working functions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // import functions and grab DOM elements
 
 import { setThreePokemon } from './utils.js';
-
+import { setPokeRecords } from './localstorageutils.js';
 
 // initialize state
-
+setPokeRecords([]);
 // set event listeners to update state and DOM
 
 setThreePokemon();
--- results/app.js
+++ results/app.js
@@ -1,7 +1,40 @@
 import { getPokeRecords } from '../localstorageutils.js';
-let pokeResults = document.getElementById('poke-results');
+import { pokemon } from '../Pokedata.js';
+import { findBySpeciesId } from '../utils.js';
 
-console.log(getPokeRecords());
 
-const pokeGetResults = JSON.stringify(getPokeRecords());
-pokeResults.textContent = pokeGetResults;
+
+const pokeGetResults = getPokeRecords();
+
+const table = document.querySelector('table');
+
+for (let item of pokeGetResults) {
+    const tableRowDOM = renderLineItems(item);
+    table.append(tableRowDOM);
+}
+
+export function renderLineItems(pokeGetResults) {
+    const encounteredA = pokeGetResults.Encountered;
+    const capturedB = pokeGetResults.Captured;
+    const tr = document.createElement('tr');
+
+    const pokeBase = findBySpeciesId(pokemon, pokeGetResults.species_id);
+
+    const idTd = document.createElement('td');
+    const encounteredTd = document.createElement('td');
+    const capturedTd = document.createElement('td');
+    const image = document.createElement('img');
+    image.src = pokeBase.url_image;
+    image.classList.add('pokemon-img');
+
+    idTd.textContent = pokeGetResults.species_id;
+    encounteredTd.textContent = encounteredA;
+    capturedTd.textContent = capturedB;
+    tr.append(idTd);
+    tr.append(encounteredTd);
+    tr.append(capturedTd);
+    tr.append(image); 
+
+
+    return tr;
+}
